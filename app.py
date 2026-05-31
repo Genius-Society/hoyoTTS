2401039819eef8ae36233169e7f65ff2ff4ba027
admin <admin@noreply.com>
2026-05-31 16:10:28 +0800

    upd theme

--- app.py
+++ app.py
@@ -317,4 +317,8 @@ if __name__ == "__main__":
                 concurrency_limit=4,
             )
 
-    app.launch(css="#gradio-share-link-button-0 { display: none; }", ssr_mode=False)
+    app.launch(
+        theme=gr.themes.Base(),
+        css="#gradio-share-link-button-0 { display: none; }",
+        ssr_mode=False,
+    )
